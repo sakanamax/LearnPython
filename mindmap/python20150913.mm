591bc30770d8446c80dcb2568bcd61cf8399d267
Max Huang <sakana@cycu.org.tw>
2015-09-13 23:02:47 -0500

    update mindmap

--- mindmap/python20150913.mm
+++ mindmap/python20150913.mm
@@ -19,5 +19,82 @@
 </richcontent>
 </node>
 </node>
+<node CREATED="1442202488273" ID="ID_1426438181" MODIFIED="1442202504779" POSITION="left" TEXT="Variables">
+<node CREATED="1442202507210" ID="ID_1028500846" MODIFIED="1442202524519" TEXT="&#x8b8a;&#x6578;&#x540d;&#x7a31; = &#x503c;">
+<node CREATED="1442202886751" ID="ID_900518442" MODIFIED="1442202909684" TEXT="01_variables.py">
+<node CREATED="1442202860101" ID="ID_81636094" MODIFIED="1442202868188">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      # &#23450;&#32681;&#35722;&#25976;
+    </p>
+    <p>
+      # &#35722;&#25976;&#21517;&#31281; = &#20540;
+    </p>
+    <p>
+      my_variable = 10
+    </p>
+    <p>
+      # &#21015;&#21360; my_variable, python 3 &#30340;print &#35201;&#21152;&#19978;&#25324;&#34399;
+    </p>
+    <p>
+      print( my_variable )
+    </p>
+    <p>
+      print( &quot;my_variable is %s&quot; % my_variable )
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1442203128087" ID="ID_80625707" MODIFIED="1442203129547" POSITION="left" TEXT="Booleans">
+<node CREATED="1442203184977" ID="ID_1485386033" MODIFIED="1442203201058" TEXT="02_booleans.py">
+<node CREATED="1442203204632" ID="ID_875790164" MODIFIED="1442203210227">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      # &#32244;&#32722;&#35373;&#23450;&#35722;&#25976;
+    </p>
+    <p>
+      # &#35373;&#23450; my_int = 7 &#39006;&#22411;&#28858; int ( &#25972;&#25976; )
+    </p>
+    <p>
+      my_int = 7
+    </p>
+    <p>
+      print( my_int )
+    </p>
+    <p>
+      # &#35373;&#23450; my_float = 1.23 &#39006;&#22411;&#28858; float ( &#28014;&#40670;&#25976; )
+    </p>
+    <p>
+      my_float = 1.23
+    </p>
+    <p>
+      print( my_float )
+    </p>
+    <p>
+      # &#35373;&#23450; my_bool = True &#39006;&#22411;&#28858; boolean ( &#24067;&#26519; )
+    </p>
+    <p>
+      my_bool = True
+    </p>
+    <p>
+      print( my_bool )
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
 </node>
 </map>

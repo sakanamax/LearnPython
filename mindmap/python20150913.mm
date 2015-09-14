cf18f56cef77b75f54dace6b87a07a9c750482e9
Max Huang <sakana@cycu.org.tw>
2015-09-13 22:23:21 -0500

    Create basic mind map

---
+++ mindmap/python20150913.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1442200812925" ID="ID_1529893442" MODIFIED="1442200828937" STYLE="bubble" TEXT="python"/>
+</map>

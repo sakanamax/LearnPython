93edbd4aaa4066997d5b9d587b60a3ae4ee2de89
Max Huang <sakana@cycu.org.tw>
2017-01-29 10:28:49 +0800

    20170129

---
+++ books/Python程式設計實務_博碩/chapter4/4-1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as pt
+x = np.arange(0,360)
+y = np.sin( x * np.pi / 180.0)
+pt.plot(x,y)
+pt.xlim(0,360)
+pt.ylim(-1.2,1.2)
+pt.title("SIN function")
+pt.show()
